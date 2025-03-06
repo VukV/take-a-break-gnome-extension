610504ea9d462d55f36447e2c7051ca04ac9c41c
VukV <vuk.vukovic.2000@gmail.com>
2025-03-06 21:02:23 +0100

    initial slider setup
    todo: work on paddings and hover selection

--- extension.js
+++ extension.js
@@ -6,6 +6,7 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const Main = imports.ui.main;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
+const Slider = imports.ui.slider;
 
 const _ = ExtensionUtils.gettext;
 const Me = ExtensionUtils.getCurrentExtension();
@@ -14,9 +15,11 @@ const ICONS_PATH = `${Me.path}/icons`;
 const TIMER_ICON_PATH = `${ICONS_PATH}/timer-symbolic.svg`;
 
 let TIMER_ACTIVE = false;
+let TIMER_DURATION = 25;
 
 const Indicator = GObject.registerClass(
     class Indicator extends PanelMenu.Button {
+
         _init() {
             super._init(0.0, _('Take A Break Indicator'));
 
@@ -32,29 +35,48 @@ const Indicator = GObject.registerClass(
 
         _initSettingsMenu() {
             let menuItem = new PopupMenu.PopupBaseMenuItem({ reactive: false });
-
             let box = new St.BoxLayout({ vertical: true, style_class: 'panel-menu' });
 
+            // Toggle switch
             this._toggleSwitch = new PopupMenu.PopupSwitchMenuItem(_('Timer Active'), false);
             this._toggleSwitch.connect('toggled', (item, state) => {
                 this._onToggleSwitch(state);
             });
-
             box.add_child(this._toggleSwitch.actor);
-            menuItem.actor.add_child(box);
 
+            // Slider for selecting time
+            this._slider = new Slider.Slider((TIMER_DURATION - 5) / 85); // Normalize from 5-90 to 0-1
+            this._slider.connect('notify::value', () => {
+                this._updateSliderValue();
+            });
+
+            let sliderItem = new PopupMenu.PopupBaseMenuItem({ reactive: false });
+            sliderItem.actor.add_child(this._slider);
+            box.add_child(sliderItem.actor);
+
+            this._sliderLabel = new St.Label({ text: `${TIMER_DURATION} min`, y_align: St.Align.MIDDLE });
+            box.add_child(this._sliderLabel);
+
+
+            menuItem.actor.add_child(box);
             return menuItem;
         }
 
         _onToggleSwitch(state) {
             TIMER_ACTIVE = state;
         }
+
+        _updateSliderValue() {
+             // Convert 0-1 range to 5-90 min
+            TIMER_DURATION = Math.round(this._slider.value * 85 / 5) * 5 + 5;
+            this._sliderLabel.text = `${TIMER_DURATION} min`;
+        }
     });
 
 class Extension {
+
     constructor(uuid) {
         this._uuid = uuid;
-
         ExtensionUtils.initTranslations(GETTEXT_DOMAIN);
     }
 
